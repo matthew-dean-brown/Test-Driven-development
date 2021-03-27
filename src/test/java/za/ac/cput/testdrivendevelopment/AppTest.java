2ca51247503ef1616d6a62259b957096f1611838
Matthew Dean Brown <mattrewbrown@gmail.com>
2021-03-27 22:49:05 +0200

    Matthew commited

--- src/test/java/za/ac/cput/testdrivendevelopment/AppTest.java
+++ src/test/java/za/ac/cput/testdrivendevelopment/AppTest.java
@@ -11,5 +11,7 @@ public class AppTest
 {
     public static void main(String[] args) {
         System.out.println("hi");
+        //Matthew(220291454) coded this below
+        System.out.println("Hi there! Coding is fun!");
     }
 }
